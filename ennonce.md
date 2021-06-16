1379533cecd113b53535170c2a562fc847819d5f
David Gilson <d.gilson@afelio.be>
2021-06-16 17:50:15 +0200

    Ennonce

---
+++ ennonce.md
@@ -0,0 +1,39 @@
+## Ennoncé de l'examen
+
+1. Afficher la liste des stagiaires:
+    - Mr ou Mme selon le sexe,
+    - le nom en majuscule,
+    - le prenom avec la première lettre en majuscule,
+    - le reste en minuscule.
+
+2. Créer une page qui détaillera un stagiaire dont la position dans le tableau sera passée en paramètre get (query param):
+    - "Aucune personne trouvée" : Si la position est en dehors du tableau.
+    - "Pas de paramètre fournis": Si le paramètre n'est pas fournis.
+    - Les Hobbies choisis seront listés par leur noms et non leur trigramme
+
+3. En cliquant sur un nom du point 1 mener à la page associée en point 2
+
+4. (Modifier) idem que sur le point 2 mais afficher dans un formulaire pré rempli
+    - afficher le champ nom prérempli avec le nom
+    - le champ prénom prérempli avec le prénom
+    - 3 champs pour la ddn : 1 champ pour le jour, une liste déroulante pour les mois, un champ pour l'année
+    - deux radio pour le sexe ()
+    - un champ pour le nombre d'enfant (prérempli)
+    - un ensemble de checkbox (tout ceux de la variable $hobbies) avec ceux de la personne déjà cochés
+
+5. Créer une page séparée pour la sauvegarde des mofifications (point 4).
+    Valider les valeurs reçues et si cela est valide afficher les données comme en point 2
+    Valider :
+    - nom doit être rempli, pas de chiffre
+    - prénom peut être rempli, pas de chiffre
+    - la date de naissance est obligatoire / être une date réelle (fonction de date)/ inférieure à aujourd'hui
+    - Le sexe est obligatoire et ne peut être que "M" ou "F"
+    - Les enfants sont optionnels mais cela doit être un entier > 0 si fournis
+    - Les hobbies sont optionnels mais doivent être dans la liste si fournis
+
+Vérifiez que votre application est sécurisée en testant les noms suivants:
+- `<i>Albert</i>`
+- `<script>alert('coucou');</script>`
+- `l'étonnant "OHOHOH"`
+
+Bon travail
---
+++ liste_stagiaires.php
@@ -0,0 +1,110 @@
+<?php
+$stagiaires = [
+    [
+        "nom" => "Gilson", 
+        "prenom" => "David", 
+        "ddn" => "28/12/1973",
+        "sexe" => "M",
+        "enfants" => 1,
+        "hobbies" => ["VID", "GUI", "PGM"] 
+    ],
+    [
+        "nom" => "Arabia",
+        "prenom" => "Jonathan",
+        "ddn" => "05/10/1993",
+        "sexe" => "M",
+        "enfants" => 0,
+        "hobbies" => ["KAR", "VEL", "PGM","MNG"]
+    ],
+    [
+        "nom" => "RALU",
+        "prenom" => "Renaud",
+        "ddn" => "05/03/1983",
+        "sexe" => "M",
+        "enfants" => 2,
+        "hobbies" => ["BSK", "PIA", "CHT"]
+    ],
+    [
+        "nom" => "Connault", 
+        "prenom" => "Clotilde", 
+        "ddn" => "23/06/1987",
+        "sexe" => "F",
+        "enfants" => 5,
+        "hobbies" => ["GUI", "JDR", "PGM"] 
+    ],
+    [
+        "nom" => "Carion", 
+        "prenom" => "Stéphan", 
+        "ddn" => "13/07/1986",
+        "sexe" => "M",
+        "enfants" => 0,
+        "hobbies" => ["PGM","PSM"] 
+    ],
+    [
+        "nom" => "Waroux", 
+        "prenom" => "Lucas", 
+        "ddn" => "13/11/1994",
+        "sexe" => "M",
+        "enfants" => 0,
+        "hobbies" => ["GAG", "TMA", "EDC"] 
+    ],
+    [
+        "nom" => "Kilesse", 
+        "prenom" => "Alysson", 
+        "ddn" => "11/09/1999",
+        "sexe" => "F",
+        "enfants" => 0,
+        "hobbies" => ["GAM", "MUS", "PGM"] 
+    ],
+    [
+        "nom" => "Piccoli", 
+        "prenom" => "Raphaël", 
+        "ddn" => "11/08/1996",
+        "sexe" => "M",
+        "enfants" => 0,
+        "hobbies" => ["GAM", "TEC", "SOR"] 
+    ],
+    [
+        "nom" => "STRYKERS",
+        "prenom" => "Simon",
+        "ddn" => "28/10/19997",
+        "sexe" => "M",
+        "enfants" => 0,
+        "hobbies" => ["JDR", "CHT", "PGM", "MNG", "VID", "PIA", "GAM", "DNS"]
+    ],
+    [
+        "nom" => "Lecocq", 
+        "prenom" => "Alexandre", 
+        "ddn" => "30/05/1996",
+        "sexe" => "M",
+        "enfants" => 0,
+        "hobbies" => ["VID", "JEU", "SER","CRI"] 
+    ]
+];
+
+$hobbies = [
+    "VID" => "Vidéo",  
+    "TEN" => "Tennis",  
+    "PGM" => "Programation", 
+    "KAR" => "Karate",
+    "VEL" => "Velo",
+    "MNG" => "Manga",
+    "BSK" => "Basketball",  
+    "PIA" => "Piano",  
+    "CHT" => "Chant",
+    "GUI" => "Guitare",  
+    "JDR" => "Jeu de rôle",
+    "PSM" => "Plongée-sous-Marine",
+    "TMA" => "Taper Mes Amis",  
+    "EDC" => "Exploiter Des Chinois",
+    "GAG" => "Gagner",
+    "GAM" => "Jeux vidéo",
+    "MUS" => "Musique",
+    "TEC" => "Technologie",
+    "SOR" => "Sortie entre amis",
+    "PIA" => "Piano",  
+    "DNS" => "Danse", 
+    "CRI" => "Crier",
+    "SER" => "Series", 
+
+];
